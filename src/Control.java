77db722b4508853c38e5294738bebf83151e6d97
Ibrahim Samir <ibrahimsamir.tec@gmail.com>
2024-12-07 11:09:55 +0200

    create project

---
+++ src/Control.java
@@ -0,0 +1,22 @@
+public class Control {
+    public void create (){
+
+    }
+
+    public int read (){
+        int any ;
+        any = 0 ;
+        return any ;
+    }
+
+    public void update (){
+        int target;
+        target = 0;
+    }
+
+    public void delete (){
+
+    }
+
+
+}

c37c7798ee8c029a13549ecdab7a35750e0317b2
EsraaNehad <esraanehad0303@gmail.com>
2024-12-10 00:00:30 +0200

    done

---
+++ src/Instructor.java
@@ -0,0 +1,21 @@
+
+public class Instructor {
+    private int id ;
+    private String instructorName;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+
+    public void setInstructorName(String instructorName){
+        this.instructorName = instructorName;
+    }
+    public String getInstructorName(){
+        return instructorName;
+    }
+}

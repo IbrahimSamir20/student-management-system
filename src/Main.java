e9c3f07e8d54340dd432244c92b65a03e0e43909
osama55585 <https://github.com/osama55585/osama_Progects.git>
2024-12-09 12:08:14 -0800

    Student class

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,6 @@ import javaapplication1.Student;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-Student r=new Student();
-         r.addname("osama");
-         r.addage(21);
-         r.addcours("software testing");
-         r.addlevel("senior");
-         
-         System.out.print(r.getviwe());
+
     }
-}
+    }

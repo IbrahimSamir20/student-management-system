38128887de6467da5548e94eb3c57f9305184bf8
Ibrahim Samir <ibrahimsamir.tec@gmail.com>
2024-12-09 18:17:41 +0200

    update for error solution

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,19 @@
 
-import javaapplication1.Student;
+// import javaapplication1.Student;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-Student r=new Student();
-         r.addname("osama");
-         r.addage(21);
-         r.addcours("software testing");
-         r.addlevel("senior");
+// Student r=new Student();
+//          r.addname("osama");
+//          r.addage(21);
+//          r.addcours("software testing");
+//          r.addlevel("senior");
          
-         System.out.print(r.getviwe());
-    }
+//          System.out.print(r.getviwe());
+// Student name = new Student();
+ 
+
+}
 }
